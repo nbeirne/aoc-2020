a28c419ce6c8cf9ab3c6fbd3a6a68ab919b15954
Nick Beirne <nbeirne@users.noreply.github.com>
2020-12-05 11:21:12 -0500

    list comprehension

--- 1/1.hs
+++ 1/1.hs
@@ -1,16 +1,18 @@
+-- naive solution
+
 solve1 :: [Int] -> Int
 solve1 lines = head [a*b | a <- lines, b <- reverse lines, a+b == 2020]
 
 solve2 :: [Int] -> Int
 solve2 lines = head [a*b*c | a <- lines, b <- lines, c <- lines, a+b+c == 2020]
 
--- more efficient answer. solve1 and solve2 work for the challenge, but this is makign those answers better
+-- more efficient answer. solve1 and solve2 work for the challenge, but this is making those answers better
 pairs :: [a] -> [(a, a)]
 pairs [] = []
 pairs (x:xs) = [(x, y) | y <- xs] ++ pairs xs
  
 solve1' :: [Int] -> Int
-solve1' = head . map (\(a,b) -> a*b) . filter (\(a,b) -> a+b == 2020) . pairs
+solve1' list = head [a*b | (a,b) <- pairs list, a+b == 2020]
 
 -- parse
 
